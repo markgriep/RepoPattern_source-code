d25d690327916f5b08860c7f7613184b9149761b
megriep <markgriep@users.noreply.github.com>
2024-09-09 13:26:53 -0500

    Add project files.

---
+++ before/gatherly/src/Gatherly.Domain/Repositories/IGatheringRepository.cs
@@ -0,0 +1,14 @@
+﻿using Gatherly.Domain.Entities;
+
+namespace Gatherly.Domain.Repositories;
+
+public interface IGatheringRepository
+{
+    Task<Gathering?> GetByIdWithCreatorAsync(Guid id, CancellationToken cancellationToken = default);
+
+    Task<Gathering?> GetByIdWithInvitationsAsync(Guid id, CancellationToken cancellationToken = default);
+
+    void Add(Gathering gathering);
+
+    void Remove(Gathering gathering);
+}
---
+++ before/gatherly/src/Gatherly.Persistence/Repository/GatheringRepository.cs
@@ -0,0 +1,27 @@
+﻿using Gatherly.Domain.Entities;
+using Gatherly.Domain.Repositories;
+
+namespace Gatherly.Persistence.Repository;
+
+internal sealed class GatheringRepository : IGatheringRepository
+{
+    public Task<Gathering?> GetByIdWithCreatorAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Gathering?> GetByIdWithInvitationsAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Add(Gathering gathering)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Remove(Gathering gathering)
+    {
+        throw new NotImplementedException();
+    }
+}
